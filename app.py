2302e2c04f34a4e5dade11a74b6a5e71f85918eb
PanCake399 <ksmile933@gmail.com>
2023-04-02 23:02:31 +0800

    minor changes

--- app.py
+++ app.py
@@ -12,3 +12,4 @@ if __name__ == '__main__':
 # Model: The data and the logic that manipulates the data. In Flask, this typically refers to the database or other data storage mechanism.
 # View: The presentation layer that handles user interaction and displays the data to the user. In Flask, this typically refers to the HTML templates that are rendered by the web server and sent to the user's browser.
 # Presenter: The intermediary component that connects the model and view components. It processes the user input, retrieves the data from the model, and passes it on to the view for display.
+# student view all the quiz
