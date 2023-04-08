7dc9601285067748f4d650e2d02eee1bbb7d1ab6
PanCake399 <ksmile933@gmail.com>
2023-04-08 22:45:39 +0800

    QuizlList table is update and Matching to multiple

--- website/models.py
+++ website/models.py
@@ -12,34 +12,35 @@ class User(db.Model, UserMixin):
     lastname = db.Column(db.String(150))
     usertype = db.Column(db.String(10))
 
-class QuizList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(20), unique=True, nullable=False)
-    title = db.Column(db.String(100), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    category = db.Column(db.String(100), nullable=False)
-    startdate = db.Column(db.Date, nullable=True)
-    starttime = db.Column(db.Time, nullable=True)
-    enddate = db.Column(db.Date, nullable=True)
-    time_closed = db.Column(db.Time, nullable=True)
-
-# class Quiz(db.Model):
+# class QuizList(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
-#     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 #     code = db.Column(db.String(20), unique=True, nullable=False)
 #     title = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.String(200), nullable=False)
-#     category = db.Column(db.String(10), nullable=False)
-#     quiztype = db.Column(db.String(10), nullable=False)
+#     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     category = db.Column(db.String(100), nullable=False)
 #     startdate = db.Column(db.Date, nullable=True)
+#     starttime = db.Column(db.Time, nullable=True)
 #     enddate = db.Column(db.Date, nullable=True)
-#     timelimit = db.Column(db.Integer, nullable=False)
-#     points = db.Column(db.Integer, nullable=False)
-#     visibility = db.Column(db.Integer, nullable=False)
+#     time_closed = db.Column(db.Time, nullable=True)
+
+class QuizList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    code = db.Column(db.String(20), unique=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    category = db.Column(db.String(10), nullable=False)
+    quiztype = db.Column(db.String(10), nullable=False)
+    startdate = db.Column(db.Date, nullable=True)
+    enddate = db.Column(db.Date, nullable=True)
+    timelimit = db.Column(db.Integer, nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+    visibility = db.Column(db.Integer, nullable=False)
+    attempt = db.Column(db.Integer, nullable=False)
 
 
-
-class MatchingType(db.Model):
+# change Matching Type into Multiple choice
+class MultipleChoice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quiz_code = db.Column(db.String(20), db.ForeignKey('quiz_list.code'), nullable=False)
     question = db.Column(db.String(255), nullable=False)
