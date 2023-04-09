bcdc39c1d672995ae0e13549a521de7f463539e1
PanCake399 <ksmile933@gmail.com>
2023-04-09 19:02:00 +0800

    fix for quizedit clipboard

--- website/static/clipboard.js
+++ website/static/clipboard.js
@@ -43,3 +43,49 @@ function copyText(quizid) {
   });
 
 }
+
+function copyCode(quizcode) {
+
+  // Get the text field
+  var copyText = document.getElementById("quizcode");
+  
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+   // var codeText= 'Dear User, \nWe would like to remind you that you have an upcoming quiz scheduled on quizguard, \nTo access your quiz, kindly visit quizguard.com \nand enter your quiz code: ' + copyText.value;
+
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  Swal.fire({
+    title: "Code Copied",
+    text: "The quiz code has been copied to your clipboard.",
+    icon: "success",
+    timer: 1000 // time in milliseconds
+  });
+}
+
+function shareCode(quizcode) {
+  // Get the text field
+  var copyText = document.getElementById("quizcode");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  var codeText= 'Dear User, \nWe would like to remind you that you have an upcoming quiz scheduled on quizguard, \nTo access your quiz, kindly visit quizguard.com \nand enter your quiz code: ' + copyText.value;
+
+  navigator.clipboard.writeText(codeText);
+
+  // Alert the copied text
+  Swal.fire({
+    title: "Code Copied",
+    text: "Share this quiz to your students.",
+    icon: "success",
+    timer: 1000 // time in milliseconds
+  });
+
+}
