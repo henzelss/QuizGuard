99e7777f7359ef26930d48e1316131bd60617eb8
PanCake399 <ksmile933@gmail.com>
2023-04-09 19:01:40 +0800

    check_category function

--- website/utils.py
+++ website/utils.py
@@ -17,3 +17,30 @@ def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join((random.choice(letters_and_digits) for i in range(length)))
 
+def check_category(category):
+    
+    if category == '1':
+        return "Language"
+    elif category == '2':
+        return "Social Studies"
+    elif category == '3':
+        return "Science"
+    elif category == '4':
+        return "History"
+    elif category == '5':
+        return "Science"
+    elif category == '6':
+        return "Physics"
+    elif category == '7':
+        return "Biology"
+    elif category == '8':
+        return "Chemistry"
+    elif category == '10':
+        return "Geography"
+    elif category == '11':
+        return "Career and Technical Education"
+    elif category == '12':
+        return "Creative Arts"
+    else:
+        return None
+    
